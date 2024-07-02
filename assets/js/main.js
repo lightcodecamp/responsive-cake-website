7c37a0083ac98966db15e16da559c4e2a3cf784f
lightcodecamp <lightcodecamp@gmail.com>
2024-07-02 22:10:46 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/*~~~~~~~~~~~~~~~ SHOW MENU ~~~~~~~~~~~~~~~*/
+
+/* Menu show */
+
+/* Menu hidden */
+
+
+/*~~~~~~~~~~~~~~~ REMOVE MENU MOBILE ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ SHADOW HEADER ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ SWIPER FAVORITES ~~~~~~~~~~~~~~~*/ 
+
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/ 
+
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
+
